ef2b889f79a62636ba8031b84e7350b5e6f7f357
OjanayR <69225985+OjanayR@users.noreply.github.com>
2020-12-28 20:32:29 -0500

    update backend functions

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,19 +1,44 @@
+import { setToken, getUserFromToken, removeToken } from './tokenService';
 const BASE_URL = 'http://localhost:3001/api/users/';
 
 function signup(user) {
-  return fetch(BASE_URL + 'signup', {
+  return fetch(BASE_URL + '/signup', {
     method: 'POST',
-    headers: new Headers({'Content-Type': 'application/json'}),
+    headers: {
+      'Content-Type': 'Application/json'
+    },
     body: JSON.stringify(user)
-  })
-  .then(res => {
-    if (res.ok) return res.json();
-    // Probably a duplicate email
-    throw new Error('Email already taken!');
-  })
-  .then(data => data);
+  }).then(response => {
+    if(response.ok) return response.json();
+    throw new Error ('Email already taken');
+  }).then(({ token }) => setToken(token));
 }
 
+function login(credentials) {
+  return fetch(BASE_URL + '/login', {
+    method: 'POST',
+    header: {
+      'Content-Type': 'Application/json'
+    },
+    body.JSON.stringify(credentials)
+  }).then(response => {
+    if(response.ok) return response.json();
+    throw new Error('Bad Credentials');
+  }).then(({ token }) => setToken(token));
+}
+
+function logout() {
+  removeToken();
+}
+
+function getUser() {
+  return getUserFromToken
+}
+
+
 export {
-  signup
+  signup,
+  login,
+  logout,
+  getUser
 };
